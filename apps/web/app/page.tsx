5baaa285ea68c621962463e5e5529c235d2ca608
FlorentinoG9 <f.valle.garcia@gmail.com>
2025-06-28 12:49:39 +0200

    init

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import { UploadFiles } from '@/components/file-upload';
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh items-center justify-center">
+      <UploadFiles />
+    </div>
+  );
+}
